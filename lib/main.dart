8c3243a9ceae2c7477800ecf5a5ebc0ab5ff04f8
Hellen <hellentrejo@gmail.com>
2020-06-22 17:00:24 -0500

    Primer commit

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'Widgets/FormCard.dart';
+
+void main() => runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
+
+  
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334, allowFontScaling: true);
+    return new Scaffold(
+        backgroundColor: Colors.white,
+        resizeToAvoidBottomPadding: true,
+        body: Stack(
+          fit:StackFit.expand,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: Image.asset("assets/image_01.png"),
+                  ),
+                  Expanded(
+                    child: Container(),
+                    ),
+                    Image.asset("assets/image_02.png")
+              ],
+            ),
+            SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.only(left:28.0, right: 28.0, top: 70.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Image.asset(
+                          "assets/logo.png",
+                          width: ScreenUtil.getInstance().setWidth(110),
+                          height: ScreenUtil.getInstance().setHeight(110),
+                        ),
+                        Text("COLE", 
+                          style: TextStyle(
+                            fontFamily: "Poppins-Bold",
+                            fontSize: ScreenUtil.getInstance().setSp(40),
+                            color: Color(0xFF280042),
+                            letterSpacing: .6,
+                            fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                    SizedBox(
+                      height: ScreenUtil.getInstance().setHeight(180),
+                    ),
+                    FormCard(),//VIENE DE LA CARPETA WIDGETS FormCard
+                    SizedBox(height: ScreenUtil.getInstance().setHeight(40)),
+                    Row(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            SizedBox(
+                              width: 12.0,
+                            ),
+                            //
+                            
+                          ],
+                        )
+                      ],
+                    ),
+                    InkWell(
+                      child: Container(
+                        width: ScreenUtil.getInstance().setWidth(330),
+                        height: ScreenUtil.getInstance().setHeight(100),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFF520082),
+                              Color(0xFF25002E)
+                            ]
+                          ),
+                          borderRadius: BorderRadius.circular(6.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xFF25002E).withOpacity(.3),
+                              offset: Offset(0.0, 8.0),
+                              blurRadius: 8.0
+                            )
+                          ]
+                        ),
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: (){},//Creo que aquí se añade el metodo
+                            child: Center(
+                              child: Text("SING IN", style: TextStyle(color: Colors.white,
+                              fontFamily: "Poppins-Bold",
+                              fontSize: 18,
+                              letterSpacing: 1.0
+                              )),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+    );
+    
+  }
+}
